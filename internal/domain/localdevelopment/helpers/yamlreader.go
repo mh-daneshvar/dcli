c5db5ac8b5e7c3583e581de6339fae72759248f2
cafeekdotcom <cafeekdotcom@gmail.com>
2024-10-27 11:17:13 +0330

    refactor(yamlreader): replace deprecated ioutil.ReadFile with os.ReadFile
    Updated the LoadProjects function to use os.ReadFile instead of the deprecated ioutil.ReadFile, in line with Go 1.16+ best practices.

--- internal/domain/localdevelopment/helpers/yamlreader.go
+++ internal/domain/localdevelopment/helpers/yamlreader.go
@@ -2,14 +2,15 @@ package helpers
 
 import (
 	"fmt"
-	"io/ioutil"
+	"os"
+
+	"gopkg.in/yaml.v3"
 
 	"github.com/mh-daneshvar/dcli/internal/domain/localdevelopment/vo"
-	"gopkg.in/yaml.v3"
 )
 
 func LoadProjects(filePath string, projects *vo.Projects) error {
-	yamlFile, err := ioutil.ReadFile(filePath)
+	yamlFile, err := os.ReadFile(filePath)
 	if err != nil {
 		return fmt.Errorf("error reading YAML file: %w", err)
 	}
